70deb3e208f8d167048683ae691f6d7fe94cb21b
yngtodd <young.todd.mk@gmail.com>
2019-04-05 21:45:32 -0400

    getting there;

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,27 @@
-pub enum List {
+pub struct List {
+    head: Link,
+}
+
+enum Link {
     Empty,
-    Elem(i32, Box<List>),
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        // TODO
+        let new_node = Node {
+            elem: elem,
+            next: self.head,
+        };
+    }
 }
