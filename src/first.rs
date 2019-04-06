73f133bd2256602fe00e212afd43b0a05f08bc18
yngtodd <young.todd.mk@gmail.com>
2019-04-06 00:48:14 -0400

    1/6 -> stack

--- src/first.rs
+++ src/first.rs
@@ -1,5 +1,42 @@
+use std::mem;
+
 pub struct List {
     head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
 }
 
 enum Link {
@@ -12,16 +49,31 @@ struct Node {
     next: Link,
 }
 
-impl List {
-    pub fn new() -> Self {
-        List { head: Link::Empty }
-    }
+#[cfg(test)]
+mod test {
+    use super::List;
 
-    pub fn push(&mut self, elem: i32) {
-        // TODO
-        let new_node = Node {
-            elem: elem,
-            next: self.head,
-        };
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list is OK
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(0);
+        list.push(1);
+        list.push(2);
+
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+
+        list.push(3);
+        list.push(4);
+
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(0));
+        assert_eq!(list.pop(), None);
     }
 }
